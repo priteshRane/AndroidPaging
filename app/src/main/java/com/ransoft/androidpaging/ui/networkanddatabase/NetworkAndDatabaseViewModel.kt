f75791492bd39c9e13d9ddd0bbef6b36da55f2f4
Pritesh Rane <priteshrane01@gmail.com>
2020-08-26 20:12:25 +0530

    Commentted Network + Database item from side nav

--- app/src/main/java/com/ransoft/androidpaging/ui/networkanddatabase/NetworkAndDatabaseViewModel.kt
+++ app/src/main/java/com/ransoft/androidpaging/ui/networkanddatabase/NetworkAndDatabaseViewModel.kt
@@ -9,15 +9,20 @@ import com.ransoft.androidpaging.data.db.AppDatabase
 import com.ransoft.androidpaging.data.db.entities.Item
 import javax.inject.Inject
 
-class NetworkAndDatabaseViewModel @Inject constructor(appDatabase: AppDatabase, itemBoundaryCallback: ItemBoundaryCallback) : ViewModel() {
+class NetworkAndDatabaseViewModel @Inject constructor(
+    appDatabase: AppDatabase,
+    itemBoundaryCallback: ItemBoundaryCallback
+) : ViewModel() {
     private var personsLiveData: LiveData<PagedList<Item>>
 
     init {
         val factory: DataSource.Factory<Int, Item> =
             appDatabase.itemDao().getAllItemFromDB()
 
-        val pagedListBuilder: LivePagedListBuilder<Int, Item> = LivePagedListBuilder<Int, Item>(factory,
-            10)
+        val pagedListBuilder: LivePagedListBuilder<Int, Item> = LivePagedListBuilder<Int, Item>(
+            factory,
+            10
+        )
         personsLiveData = pagedListBuilder.setBoundaryCallback(itemBoundaryCallback).build()
     }
 
